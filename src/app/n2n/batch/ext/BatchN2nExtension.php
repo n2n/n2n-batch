802f20caf2672031385189ff89b1f12d3c35d66e
andreas <a@von-burg.net>
2026-07-02 17:16:15 +0200

    BatchN2nExtension fix

--- src/app/n2n/batch/ext/BatchN2nExtension.php
+++ src/app/n2n/batch/ext/BatchN2nExtension.php
@@ -5,10 +5,12 @@ namespace n2n\batch\ext;
 use n2n\core\config\AppConfig;
 use n2n\core\cache\AppCache;
 use n2n\core\container\impl\AppN2nContext;
+use n2n\core\ext\ConfigN2nExtension;
+use n2n\core\N2nApplication;
 
-class BatchN2nExtension implements \n2n\core\ext\N2nExtension {
+class BatchN2nExtension implements ConfigN2nExtension {
 
-	public function __construct(AppConfig $appConfig, AppCache $appCache) {
+	function __construct(N2nApplication $n2nApplication) {
 	}
 
 	function applyToN2nContext(AppN2nContext $appN2nContext): void {
